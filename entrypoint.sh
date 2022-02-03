2eb1c83c68072d812166dada3e723471dd03cc86
harada-yoshitsugu <harada-yoshitsugu@cyber-z.co.jp>
2022-02-04 00:36:13 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker_run="docker-compose up -d"
+docker_run="docker run -p 9200:9200 -p 9600:9600 -e "discovery.type=single-node" opensearchproject/opensearch:latest"
 sh -c "$docker_run"
