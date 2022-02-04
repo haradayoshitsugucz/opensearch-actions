8cd80ae98e2e4dc091dfa84785c67315d36b00e4
harada-yoshitsugu <harada-yoshitsugu@cyber-z.co.jp>
2022-02-04 17:50:26 +0900

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 docker_run="ddocker-compose -f ./docker-compose.yml up -d"
 
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
 which docker
